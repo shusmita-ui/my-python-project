c4c281ab790901f1d2d5da0e525a4e53026fa143
shusmita-ui <shusmitapal46@gmail.com>
2025-04-20 12:25:22 +0530

    Initial commit

---
+++ age.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+
+birth_year = int(input("Enter your birth year: "))
+current_year = datetime.now().year
+
+age = current_year - birth_year
+print("Your age", age, "years old.")
